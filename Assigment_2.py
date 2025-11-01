c4f129e75955cf14799eae13d8fc9cfec7501e63
James Novak <indianajames24@gmail.com>
2025-11-01 18:33:09 -0500

    Compled Assignment 2: Corrected functions for velocity and position calculations, added user input for height, and improved plotting with labels, grid, and legend.

---
+++ Assigment_2.py
@@ -0,0 +1,26 @@
+from matplotlib.pylab import *
+
+g = 9.81 #Acceleration due to gravity in m/s^2
+
+def velocity(t):
+    v = -g *t
+    return v
+
+def position(t,height):
+    y = height - 0.5 * g * t**2
+    return y
+
+def main():
+    height = float(input("Enter the hieght in meter from which the obect is dropped: "))
+    t = linspace(0, sqrt(2 * height /g), 100)
+    y = position(t,height)
+    v = velocity(t)
+    plot(t, y, label = 'Position (m)', color = 'blue')
+    plot(t, v, label = 'Velocity (m/s)', color = 'red')
+    title('Free Fall from Height of {} meters'.format(height))
+    xlabel('Time (s)')
+    ylabel('Position (m) and Velocity (m/s)')
+    grid(True)
+    legend()
+    show()
+main()
